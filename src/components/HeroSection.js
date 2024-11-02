655c9551063ff9aa70e2a43447c5fca801d4fc44
SURYANSH SRIVASTAVA <dhawalsri77@gmail.com>
2024-11-02 18:50:37 +0530

    Initial Commit69

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './HeroSection.css';
 
-const HeroSection = () => (
-  <section id="home" className="hero">
-    <div className="hero-content">
-      <h1 className="hero-title">Welcome to My Portfolio</h1>
-      <p className="hero-description">Highlighting my work and projects</p>
-      <a href="#portfolio" className="btn">View My Work</a>
-    </div>
-  </section>
-);
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Welcome to Cyber-Sentinel</h1>
+        <p className="hero-description">Your safety is our priority!</p>
+        <a href="#about" className="btn">Learn More</a>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection;
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import './Navbar.css'; // We'll create this CSS file later for styling
+import React, { useState } from 'react';
+import './Navbar.css';
+
+// Make sure to import the video file correctly
+import video from '../assets/857195-hd_1280_720_25fps.mp4';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const toggleMenu = () => {
-    document.querySelector('.nav-links').classList.toggle('active');
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <nav>
       <div className="logo">𝙲𝚢𝚋𝚎𝚛-𝚂𝚎𝚗𝚝𝚒𝚗𝚎𝚕</div>
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="menu-toggle" onClick={toggleMenu}>☰</div>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`slide-menu ${isMenuOpen ? 'active' : ''}`}>
+        <video className="background-video" autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="overlay"></div>
+        <ul className="nav-links">
+          <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+          <li><a href="#about" onClick={toggleMenu}>About</a></li>
+          <li><a href="#portfolio" onClick={toggleMenu}>Portfolio</a></li>
+          <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
+        </ul>
+      </div>
     </nav>
   );
 };
